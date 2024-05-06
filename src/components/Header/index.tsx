78911455c608b8dfb4f29ffbf7b8ca7bb06f9e83
isupov <t.1113@ya.ru>
2024-05-06 18:18:59 +0300

    Added fonts, normalize.css, added a possibility to use module css

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import styles from "./styles.module.css";
+
+export const Header = () => {
+    return (
+        <header className={ styles.header }>
+            Header
+        </header>
+    )
+}
