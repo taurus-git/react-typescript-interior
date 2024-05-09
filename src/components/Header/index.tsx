fa229c85b9da8d85f5bbac321052b807c5577318
isupov <t.1113@ya.ru>
2024-05-09 12:15:35 +0300

    Added basic div with classes

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -3,8 +3,13 @@ import styles from "./styles.module.css";
 
 export const Header = () => {
     return (
-        <header className={ styles.header }>
-            Header
+        <header className={ styles['header'] }>
+            <div className={ styles['header-wrapper'] }>
+                <div className={ styles['header__logo'] }></div>
+                <nav className={ styles['header__menu'] }></nav>
+                <div className={ styles['header__actions'] }></div>
+                <div className={ styles['header__auth-buttons'] }></div>
+            </div>
         </header>
     )
 }
